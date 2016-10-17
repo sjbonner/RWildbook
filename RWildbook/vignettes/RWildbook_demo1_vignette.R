01ea1078828e667d7a4156578f29c6282e1bfd5d
Simon Bonner <sbonner6@uwo.ca>
2016-10-16 22:03:03 -0400

    Normalize all the line endings

---
+++ RWildbook/vignettes/RWildbook_demo1_vignette.R
@@ -0,0 +1,16 @@
+## ---- eval=FALSE---------------------------------------------------------
+#  data1 <- searchWB(searchURL = "http://xinxin:changeme@whaleshark.org/api/jdoql?SELECT FROM org.ecocean.Encounter WHERE individualID == 'A-001'")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  data2<- searchWB(username="xinxin",
+#                   password="changeme",
+#                   baseURL ="whaleshark.org",
+#                   jdoql="SELECT FROM org.ecocean.Encounter WHERE individualID == 'A-001'")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  data3 <- searchWB(username="xinxin",
+#                    password="changeme",
+#                    baseURL ="whaleshark.org",
+#                    object="Encounter",
+#                    individualID=c("A-001"))
+
