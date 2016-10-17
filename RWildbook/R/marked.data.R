f8e65bf1a709eebbc896567b4f244ccc0e68944c
Simon Bonner <sbonner6@uwo.ca>
2016-10-16 22:01:49 -0400

    Added running of CJS model in marked to example in marked.data.

--- RWildbook/R/marked.data.R
+++ RWildbook/R/marked.data.R
@@ -43,23 +43,39 @@
 #'
 #' @examples
 #'
+#' ## Load packages
+#' library(marked)
+#' 
+#' ## Extract data for individual A-001 through A-099
 #' data1 <- searchWB(username="xinxin",
 #'                   password="changeme",
 #'                   baseURL ="whaleshark.org",
 #'                   object="Encounter",
-#'                   individualID=c("A-001","A-002","A-003"))
+#'                   individualID=paste0("A-0",rep(0:9,rep(10,10)),rep(0:9,10))[-1])
 #'
-#'start.dates1 <- paste0(1998:2016,"-01-01") #Define the start.date value
-#'end.dates1 <- paste0(1998:2016,"-04-01") #Define the end.date value
+#' ## Define start and end dates of capture occasions
+#' start.dates1 <- paste0(1998:2016,"-01-01") #Define the start.date value
+#' end.dates1 <- paste0(1998:2016,"-04-01") #Define the end.date value
 #'
-#'marked.data1.1 <- marked.data(data = data1,
-#'                              varname_of_capturetime = "dateInMilliseconds",
-#'                              varlist = c("individualID"),
-#'                              start.dates = start.dates1,
-#'                              end.dates = NULL,
-#'                              date_format = "%Y-%m-%d",
-#'                              origin = "1970-01-01")
+#' ## Format data for use in marked
+#' marked.data1.1 <- marked.data(data = data1,
+#'                               varname_of_capturetime = "dateInMilliseconds",
+#'                               varlist = c("individualID"),
+#'                               start.dates = start.dates1,
+#'                               end.dates = NULL,
+#'                               date_format = "%Y-%m-%d",
+#'                               origin = "1970-01-01")
+#'                           
+#' ## Remove 0 histories
+#' zeros = which(marked.data1.1$ch==paste0(rep(0,19),collapse=""))
+#' marked.data1.1 = marked.data1.1[-zeros,]
+#'            
+#' ## Fit simple CJS model in marked                                                         
+#' marked.data1.proc=process.data(marked.data1.1,model="cjs",begin.time=1)
+#' marked.data1.ddl=make.design.data(marked.data1.proc)
+#' marked.data1.cjs=crm(marked.data1.proc,marked.data1.ddl,model.parameters=list(Phi=list(formula=~time),p=list(formula=~time)))
 #'
+#' ## Format data including location as a covariate
 #'marked.data1.2 <- marked.data(data = data1,
 #'                              varname_of_capturetime = "dateInMilliseconds",
 #'                              varlist = c("individualID","locationID"),
